df4090a17552493a3d6761615c1c1ca7f3895d63
Lars Andersen <larsmew@gmail.com>
2017-03-15 00:27:17 +0100

    Added R script for backing up R packages

---
+++ updateR.R
@@ -0,0 +1,20 @@
+args = commandArgs(trailingOnly=TRUE)
+
+backupPackages <- function(filename = "R_packages"){
+    tmp <- installed.packages()
+    
+    # Save list as R object for easy reinstall
+    installedpkgs <- as.vector(tmp[,"Package"])
+    save(installedpkgs, file=paste0(filename, ".rda"))
+    
+    # Save list of packages and version number as text file
+    installedpkgs <- tmp[,c("Package", "Version")]
+    write.table(installedpkgs, file=paste0(filename, ".txt"), row.names = F, col.names = F, sep = "=", quote = F)
+}
+
+if (length(args) == 1){
+    filename = args[1]
+} else {
+    filename = "R_packages"
+}
+backupPackages(filename)
